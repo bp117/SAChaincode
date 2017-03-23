0beb7cc374317962c39c277860862d2f7c418c13
pigbar@gmail.com <pigbar@gmail.com>
2017-03-23 09:06:25 -0600

    updating log data

--- gomaster/gomaster.go
+++ gomaster/gomaster.go
@@ -1,117 +1,124 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	b64 "encoding/base64"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
 // WFChaincode example simple Chaincode implementation
 type WFChaincode struct {
 }
 
-func (t *WFChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
+func (t *WFChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("ex02 Init")
-	_, args := stub.GetFunctionAndParameters()
 	var err error
 	if len(args) != 1 {
-			return shim.Error("Incorrect number of arguments. Expecting 1")
+			return nil, errors.New("Incorrect number of arguments. Expecting 1")
 		}
 	
 	// Initialize the chaincode
 	err = stub.PutState("init_wf", []byte(args[0]))
 	if err != nil {
-		return shim.Error(err.Error())
+		return nil, err
 	}
 	
-	return shim.Success(nil)
+	return nil, nil
 }
 
-func (t *WFChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Println("ex02 Invoke")
-	function, args := stub.GetFunctionAndParameters()
-	if function == "write" {
+func (t *WFChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("invoke is running " + function)
+
+	if function == "init" {
+		// calls our init method
+		return t.Init(stub, "init", args)
+	} else if function == "write" {
 		// calls the write method
 		return t.write(stub, args)
-	} else if function == "delete" {
-		// Deletes an entity from its state
-		return t.delete(stub, args)
-	} else if function == "query" {
-		// the old "Query" is now implemtned in invoke
-		return t.query(stub, args)
-	}
+	} 
 
-	return shim.Error("Invalid invoke function name. Expecting \"write\" \"delete\" \"query\"")
+	fmt.Println("invoke did not find func: " + function)
+	return nil, errors.New("Received unknown function invocation: " + function + "expecting init, write, query")
 }
 
 
 // Writes an entity to state
-func (t *WFChaincode) write(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 2 {
-		return shim.Error("Incorrect number of arguments. Expecting 2, Key and Value")
+func (t *WFChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	if len(args ) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2, Key and Value")
 	}
-	var Key, Value string
+	var key, value string
 	var err error
+	var logData []byte
 	
-	Key = args[0]
-	Value = args[1]
+	key = args[0]
+	value = args[1]
+	logData, _ =  b64.StdEncoding.DecodeString(args[2])
 
-	// Write the key tothe state in ledger
-	err = stub.PutState(Key, []byte(Value))
+	fmt.Printf("Running WRITE function :%s\n", string(logData))
+	// Write the key to the state in ledger
+	err = stub.PutState(key, []byte(value))
 	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to write-put state\",\"Key\":\"" + Key + "\",\"Value\":\"" + string(Value) + "\"}"
-		return shim.Error(jsonResp)
+		jsonResp := "{\"Error\":\"Failed to write-put state\",\"Key\":\"" + key + "\",\"Value\":\"BLOCK DATA\"}"
+		return nil, errors.New(jsonResp)
 	}
-	jsonResp := "{\"Key\":\"" + Key + "\",\"Value\":\"" + string(Value) + "\"}"
+	jsonResp := "{\"Key\":\"" + key + "\",\"Value\":\"BLOCK DATA\"}"
 	fmt.Printf("Write Response:%s\n", jsonResp)
-	return shim.Success(nil)
-}
-
-// Deletes an entity from state
-func (t *WFChaincode) delete(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 1 {
-		return shim.Error("Incorrect number of arguments. Expecting 1")
-	}
-	var Key string
-	var err error
-	
-	Key = args[0]
-
-	// Delete the key from the state in ledger
-	err = stub.DelState(Key)
-	if err != nil {
-		return shim.Error("Failed to delete state")
-	}
-
-	return shim.Success(nil)
+	return nil, nil
 }
 
 // query callback representing the query of a chaincode
-func (t *WFChaincode) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var Key string // Entities
-	var err error
-
+func (t *WFChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("query is running " + function)
+	
 	if len(args) != 1 {
-		return shim.Error("Incorrect number of arguments. Expecting name of the Key to query")
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the Key to query")
 	}
-
-	Key = args[0]
 
 	// Get the state from the ledger
-	Avalbytes, err := stub.GetState(Key)
+	if function == "read" { 
+		
+		Avalbytes, err := t.read(stub, args)
+		if err != nil {
+			return nil, err
+		}
+
+		if Avalbytes == nil {
+			jsonResp := "{\"Error\":\"Nil value for " + args[0] + "\"}"
+			return nil, errors.New(jsonResp)
+		}
+
+		jsonResp := "{\"Key\":\"" + args[0] + "\",\"Value\":\"BLOCK DATA\"}"
+		fmt.Printf("Query Response: %s\n", jsonResp)
+		return Avalbytes, nil
+	}
+	fmt.Println("query did not find func: " + function)
+
+    return nil, errors.New("Received unknown function query: " + function)
+}
+
+// read - query function to read key/value pair
+func (t *WFChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+	var logData []byte
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key and log data for the query")
+	}
+
+	key = args[0]
+	logData, _ = b64.StdEncoding.DecodeString(args[1])
+	fmt.Printf("Running READ function :%s\n", string(logData))
+	// reading the state
+	valAsbytes, err := stub.GetState(key)
 	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to get state for " + Key + "\"}"
-		return shim.Error(jsonResp)
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
 	}
 
-	if Avalbytes == nil {
-		jsonResp := "{\"Error\":\"Nil value for " + Key + "\"}"
-		return shim.Error(jsonResp)
-	}
-
-	jsonResp := "{\"Key\":\"" + Key + "\",\"Value\":\"" + string(Avalbytes) + "\"}"
-	fmt.Printf("Query Response:%s\n", jsonResp)
-	return shim.Success(Avalbytes)
+	return valAsbytes, nil
 }
 
 func main() {
